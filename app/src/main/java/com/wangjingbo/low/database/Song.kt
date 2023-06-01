b3b0b7ae9a23cd83fdbd53bdcd84cbbbd6002a5f
sora <github@resoras.com>
2023-06-01 10:30:10 +0800

    简单优化

--- app/src/main/java/com/wangjingbo/low/database/Song.kt
+++ app/src/main/java/com/wangjingbo/low/database/Song.kt
@@ -1,20 +1,3 @@
 package com.wangjingbo.low.database
 
-//import androidx.room.ColumnInfo
-//import androidx.room.Entity
-//import androidx.room.PrimaryKey
-//
-//@Entity(tableName = "song")
-//data class Song(
-//    @PrimaryKey val id: Int,
-//    val name: String,
-//    val duration: Int,
-//    val mvid: Int,
-//    val fee: Int,
-//    val status: Int,
-//    @ColumnInfo(name = "album_id") val albumId: Int,
-//    @ColumnInfo(name = "artist_id") val artistId: Int,
-//    @ColumnInfo(name = "pic_url") val picUrl: String?,
-//    @ColumnInfo(name = "img1v1_url") val img1v1Url: String?
-//)
 data class Song(val url: String, val name: String, val artist: String, val album: String)
